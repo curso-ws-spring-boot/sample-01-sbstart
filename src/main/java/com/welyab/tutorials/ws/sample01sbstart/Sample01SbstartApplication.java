646c35d5cd803bab9201599815448b6184456967
Welyab Paula <welyab.exe@gmail.com>
2018-10-15 11:14:13 -0300

    initial commit

---
+++ src/main/java/com/welyab/tutorials/ws/sample01sbstart/Sample01SbstartApplication.java
@@ -0,0 +1,12 @@
+package com.welyab.tutorials.ws.sample01sbstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample01SbstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample01SbstartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/welyab/tutorials/ws/sample01sbstart/Sample01SbstartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.welyab.tutorials.ws.sample01sbstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sample01SbstartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
